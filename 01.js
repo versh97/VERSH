44398ad23631153763cfb113be9f320610ec479f
Cristian <cdvr10245@gmail.com>
2023-01-11 15:14:56 -0500

    el primero

--- 01.js
+++ 01.js
@@ -11,6 +11,9 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  return array.filter(function(element) {
+    return typeof element === 'number';
+  });
 
 }
 
